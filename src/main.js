707e85de4dd668fbd148a45fd7d2a7e36b5711ae
YounesAmraoui <48179472+YounesAmraoui@users.noreply.github.com>
2021-04-15 16:45:18 +0200

    add bootsrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
-
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
 
 Vue.config.productionTip = false
 
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import  'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 new Vue({
   router,
   render: h => h(App),
